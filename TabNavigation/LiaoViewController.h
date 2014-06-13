3a24375dbd20b636a8654d8dc33cc6e0af31fb15
patrickios <patrick_ios@163.com>
2014-06-13 21:35:56 +0800

    初次上传代码

---
+++ TabNavigation/LiaoViewController.h
@@ -0,0 +1,13 @@
+//
+//  LiaoViewController.h
+//  TabNavigation
+//
+//  Created by Patrick on 14-6-13.
+//  Copyright (c) 2014年 Patrick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LiaoViewController : UIViewController
+
+@end
---
+++ TabNavigationTests/TabNavigationTests.m
@@ -0,0 +1,34 @@
+//
+//  TabNavigationTests.m
+//  TabNavigationTests
+//
+//  Created by Patrick on 14-6-13.
+//  Copyright (c) 2014年 Patrick. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TabNavigationTests : XCTestCase
+
+@end
+
+@implementation TabNavigationTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
